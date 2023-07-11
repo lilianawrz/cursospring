1e1501a619392d21cf86616f8bfa72b1fb34c11d
Liliana Wrzesinski <114515751+lilianawrz@users.noreply.github.com>
2023-07-11 10:07:59 -0300

    Service layer, component registration

---
+++ src/main/java/com/awis/cursospring/services/UserService.java
@@ -0,0 +1,29 @@
+package com.awis.cursospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.awis.cursospring.entities.User;
+import com.awis.cursospring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	// retornar todos os usuários do banco
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	// retorna o usuário por id
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
