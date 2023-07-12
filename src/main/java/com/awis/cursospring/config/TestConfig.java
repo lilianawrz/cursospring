8a55adbc95827fc646ef775661af0b501b0b5bd8
Liliana Wrzesinski <114515751+lilianawrz@users.noreply.github.com>
2023-07-12 14:13:28 -0300

    Payment, one-to-one association

--- src/main/java/com/awis/cursospring/config/TestConfig.java
+++ src/main/java/com/awis/cursospring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.awis.cursospring.entities.Category;
 import com.awis.cursospring.entities.Order;
 import com.awis.cursospring.entities.OrderItem;
+import com.awis.cursospring.entities.Payment;
 import com.awis.cursospring.entities.Product;
 import com.awis.cursospring.entities.User;
 import com.awis.cursospring.entities.enums.OrderStatus;
@@ -75,6 +76,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1 );
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/awis/cursospring/entities/Order.java
+++ src/main/java/com/awis/cursospring/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.awis.cursospring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,14 +31,16 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	private Integer orderStatus;
-	
+
 	@ManyToOne // implementa uma chave estrangeira no banco
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	
-	@OneToMany(mappedBy= "id.order")
-	private Set <OrderItem> items = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	//mapear para ter o mesmo id
+	@OneToOne	(mappedBy="order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 
@@ -46,7 +50,7 @@ public class Order implements Serializable {
 		super();
 		this.id = id;
 		this.moment = moment;
-		setOrderStatus( orderStatus);
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -84,9 +88,19 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-public Set<OrderItem>getItems(){
-	return items;
-}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
