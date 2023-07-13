1b02de287bf255d64943e281609560182ccb6457
Liliana Wrzesinski <114515751+lilianawrz@users.noreply.github.com>
2023-07-13 10:11:18 -0300

    Exception handling - findById

--- src/main/java/com/awis/cursospring/services/UserService.java
+++ src/main/java/com/awis/cursospring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.awis.cursospring.entities.User;
 import com.awis.cursospring.repositories.UserRepository;
+import com.awis.cursospring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 
 //User insert
